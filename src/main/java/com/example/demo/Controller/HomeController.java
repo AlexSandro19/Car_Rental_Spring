1f5db32becf61a88155055c835b4e6e6fe303ac4
Dorin <jtrettore@gmail.com>
2020-05-01 19:42:34 +0200

    Customers Display
    clicking on customers on index now displays all customers and their fields

--- src/main/java/com/example/demo/Controller/HomeController.java
+++ src/main/java/com/example/demo/Controller/HomeController.java
@@ -1,7 +1,13 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Model.Customer;
+import com.example.demo.Service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 
 @Controller
@@ -12,8 +18,13 @@ public class HomeController {
         return "home/index";
     }
 
+    @Autowired
+    CustomerService customerService;
+
     @GetMapping("/customers")
-    public String create(){
+    public String customers(Model model){
+        List<Customer> customerList = customerService.fetchAll();
+        model.addAttribute("customers", customerList);
         return "home/customers";
     }
 
--- src/main/java/com/example/demo/Service/CustomerService.java
+++ src/main/java/com/example/demo/Service/CustomerService.java
@@ -1,4 +1,19 @@
 package com.example.demo.Service;
 
+import com.example.demo.Model.Customer;
+import com.example.demo.Repository.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
 public class CustomerService {
+    @Autowired
+    CustomerRepo customerRepo;
+
+    public List<Customer> fetchAll(){
+        return customerRepo.fetchAll();
+    }
+
+
 }
